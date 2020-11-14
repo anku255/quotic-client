26734f7bd166a9e39626b8631ef1942577de1510
Ankit Tiwari <ankitt255@gmail.com>
2020-11-14 15:11:29 +0530

    :bug: Fix server url

--- lib/apolloClient.ts
+++ lib/apolloClient.ts
@@ -8,7 +8,7 @@ function createApolloClient(): ApolloClient<NormalizedCacheObject> {
   return new ApolloClient({
     ssrMode: typeof window === "undefined",
     link: new HttpLink({
-      uri: "http://localhost:8000/graphql", // Server URL (must be absolute)
+      uri: process.env.NEXT_PUBLIC_API_URL, // Server URL (must be absolute)
       credentials: "same-origin", // Additional fetch() options like `credentials` or `headers`
     }),
     cache: new InMemoryCache({
